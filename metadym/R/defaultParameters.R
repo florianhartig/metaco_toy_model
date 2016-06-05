651d9a3f18a6b24d90a65fc556fa06b5a980d2d8
Florian Hartig <florianhartig@users.noreply.github.com>
2016-06-05 15:08:31 +0200

    changed default parameters

---
+++ metadym/R/defaultParameters.R
@@ -0,0 +1,56 @@
+#' Return default parameters
+getDefaultParameters <- function(){
+  
+  # Species parameters
+  
+  # u_c : matrix of dimension D X R with niche optima for each species and environmental variable for colonization
+  
+  u_c = matrix(nr = D, nc = R)
+  u_c[1,] = seq(0.1,0.9,length=R)
+  u_c[2,] = rep(0.5, R)
+  
+  # u_e : matrix of dimension D X R with niche optima for each species and environmental variable for extinction
+  
+  u_e = matrix(nr = D, nc = R)
+  u_e[1,] = seq(0.1,0.9,length=R)
+  u_e[2,] = rep(0.5, R)
+  
+  # s_c : matrix of dimension D X R with niche breadth for each species and environmental variable for colonization
+  
+  s_c = matrix(nr = D, nc = R)
+  s_c[1,] = rep(0.3, R)
+  s_c[2,] = rep(10, R)
+  
+  # s_e : matrix of dimension D X R with niche breadth for each species and environmental variable for extinction
+  
+  s_e = matrix(nr = D, nc = R)
+  s_e[1,] = rep(0.3, R)
+  s_e[2,] = rep(10, R)
+  
+  # A: matrix of dimension R X R with the effect of each species (column) on other species (rows)
+  
+  A = matrix(0, nr = R, nc = R)
+  diag(A) = 0
+  
+  pars = list(u_c = u_c, 
+              u_e = u_e, 
+              s_c = s_c, 
+              s_e = s_e, 
+              alpha = 0.1, # alpha : scalar, average dispersal distance
+              m = 0.001, # m : scalar, immigration probability
+              
+              c_0 = rep(2, R), 
+              e_0 = rep(0.02, R), # e_0 : vector of dimension R with extinction probability at 0 interaction
+              c_max = rep(0.8, R), # c_max: vector of dimension R maximal colonization probability at infinite positive interactions
+              e_min = rep(0.05, R), # e_min: vector of dimension R maximal extinction probability at infinite positive interactions
+              
+              d_c = 0.5, # d_c: scalar, strength of the effect of interactions on the colonization probability
+              d_e = 0.5, # d_e: scalar, strength of the effect of interactions on the extinction probability
+              A = A, 
+              SA = NULL # SA: spatial autocorrelation - Null defaults to no spatial autocorrelation. If a numeric value is provided, it will create an Environment with SA of the given length. 
+  )
+  
+  return(pars)
+
+}
+
